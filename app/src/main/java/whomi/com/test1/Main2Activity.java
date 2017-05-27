c9858408f76352b3b023906670afcf8e9cf66608
Hunt2176 <lawson.forbus@outlook.com>
2017-05-27 16:06:29 -0500

    Removes progress bar that had no purpose.

--- app/src/main/java/whomi/com/test1/Main2Activity.java
+++ app/src/main/java/whomi/com/test1/Main2Activity.java
@@ -2,14 +2,9 @@ package whomi.com.test1;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.design.widget.TabItem;
-import android.support.design.widget.TabLayout;
-import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.InputType;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -290,7 +285,7 @@ public class Main2Activity extends AppCompatActivity {
             }
             return;
         }
-        if (GetTextView().length()>=15){
+        if (GetTextView().length()>=10){
             return;
         }
 
